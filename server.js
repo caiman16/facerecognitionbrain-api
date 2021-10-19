eecd7c88950afe1834e05f06ae73616c2c6b7125
caiman16 <alberto.restrepo.lozano@gmail.com>
2021-10-19 17:45:32 -0400

    Connecting database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ import { handleAPICall, handleImage } from "./controllers/image.js";
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : '235primo',
-        database : 'smart-brain'
+        connectionString : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
